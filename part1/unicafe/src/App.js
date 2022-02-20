18912cc52dac9210d98ceeb5be8b6e54db313344
Henry Winters <hlwinters17@gmail.com>
2022-02-19 21:37:06 -0800

    Completed exercise 1.6 unicafe step1

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -6,11 +6,61 @@ const App = () => {
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
+  const increaseGood = () => setGood(good + 1)
+  const increaseNeutral = () => setNeutral(neutral + 1)
+  const increaseBad = () => setBad(bad + 1)
+
   return (
     <div>
-      code here
+        <Header 
+            text="give feedback"
+        />
+        <Buttons 
+            onClick={increaseGood}
+            text="good"
+        />
+        <Buttons 
+            onClick={increaseNeutral}
+            text="neutral"
+        />
+        <Buttons 
+            onClick={increaseBad}
+            text="bad"
+        />
+        <Header 
+            text="statistics"
+        />
+        <Display 
+            text="good"
+            count={good}
+        />
+        <Display 
+            text="neutral"
+            count={neutral}
+        />
+        <Display
+            text="bad"
+            count={bad}
+        />
     </div>
   )
 }
+
+const Buttons = ({ onClick, text }) => (
+    <button onClick={onClick}>
+        {text}
+    </button>
+)
+
+const Header = ({text}) => (
+    <h1>{text}</h1>
+)
+
+const Display = ({text, count}) => {
+    return (
+         <p>{text} {count}</p>
+    )
+}
+
 
 export default App
