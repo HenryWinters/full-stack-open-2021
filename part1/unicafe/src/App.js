3f09b0c9c4e497c247b50de1bfa1ad6c98f0c82b
Henry Winters <hlwinters17@gmail.com>
2022-02-20 18:22:08 -0800

    Completed exercsises 1.8 and 1.9 by adding conditional rendering to Statistics components

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -5,57 +5,88 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  
+  const all = good + neutral + bad
+  const avg = ((good*1)+(bad*-1))/all 
+  const positive = (good/all)*100+"%"
 
   const increaseGood = () => setGood(good + 1)
   const increaseNeutral = () => setNeutral(neutral + 1)
   const increaseBad = () => setBad(bad + 1)
 
-  return (
-    <div>
-        <Header 
-            text="give feedback"
-        />
-        <Buttons 
-            onClick={increaseGood}
-            text="good"
-        />
-        <Buttons 
-            onClick={increaseNeutral}
-            text="neutral"
-        />
-        <Buttons 
-            onClick={increaseBad}
-            text="bad"
-        />
-        <Header 
-            text="statistics"
-        />
-        <Display 
-            text="good"
-            count={good}
-        />
-        <Display 
-            text="neutral"
-            count={neutral}
-        />
-        <Display
-            text="bad"
-            count={bad}
-        />
-        <Display 
-            text="all"
-            count={good+neutral+bad}
-        />
-        <Display
-            text="average"
-            count={((good*1)+(bad*-1))/(good+neutral+bad)}
-        />
-        <Display
-            text="positive"
-            count={(good/(good+neutral+bad))*100+"%"}
-        />
-    </div>
-  )
+
+  if (all === 0) {
+      return ( 
+        <div> 
+            <Header 
+                text="give feedback"
+            />
+            <Buttons 
+                onClick={increaseGood} 
+                text="good"
+            />
+            <Buttons 
+                onClick={increaseNeutral}
+                text="neutral"
+            />
+            <Buttons 
+                onClick={increaseBad}
+                text="bad"
+            />
+            <Header 
+                text="statistics"
+            />
+            <p>No feedback given</p>
+        </div> 
+      )
+  }
+
+    return (
+        <div>
+            <Header 
+                text="give feedback"
+            />
+            <Buttons 
+                onClick={increaseGood} 
+                text="good"
+            />
+            <Buttons 
+                onClick={increaseNeutral}
+                text="neutral"
+            />
+            <Buttons 
+                onClick={increaseBad}
+                text="bad"
+            />
+            <Header 
+                text="statistics"
+            />
+            <Statistics 
+                text="good"
+                count={good}
+            />
+            <Statistics 
+                text="neutral"
+                count={neutral}
+            />
+            <Statistics
+                text="bad"
+                count={bad}
+            />
+            <Statistics 
+                text="all"
+                count={all}
+            />
+            <Statistics
+                text="average"
+                count={avg}
+            />
+            <Statistics
+                text="positive"
+                count={positive}
+            />
+        </div>
+    )
 }
 
 const Buttons = ({ onClick, text }) => (
@@ -68,11 +99,9 @@ const Header = ({text}) => (
     <h1>{text}</h1>
 )
 
-const Display = ({text, count}) => {
-    return (
-         <p>{text} {count}</p>
-    )
-}
+const Statistics = ({text, count}) => (
+    <p>{text} {count}</p>
+)
 
 
 export default App
