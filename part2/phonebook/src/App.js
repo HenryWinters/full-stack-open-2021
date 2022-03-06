454257564c268d8ab7bdb0f783b24d723bb5a466
Henry Winters <hlwinters17@gmail.com>
2022-03-06 13:34:53 -0800

    Added adding name functionality to phonebook to complete exercise 2.6

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import Person from './Components/Person'
 
 const App = () => {
   const [persons, setPersons] = useState([
@@ -6,19 +7,33 @@ const App = () => {
   ]) 
   const [newName, setNewName] = useState('')
 
+  
+  const addPerson = (event) => {
+    event.preventDefault();
+    setPersons(persons.concat({name: newName}))
+  }
+
+  const handleNameChange = (event) => {
+    setNewName(event.target.value)
+  }
+
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={addPerson}>
         <div>
-          name: <input />
+          name: <input value={newName} onChange={handleNameChange} />
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      ...
+      <ul>
+        {persons.map(person =>  
+          <Person key={person.name} person={person}/>
+        )}  
+      </ul> 
     </div>
   )
 }
