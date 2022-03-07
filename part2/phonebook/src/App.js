4fa32b9ca0e53ee4428a3359dd1995fd4ff95fe6
Henry Winters <hlwinters17@gmail.com>
2022-03-06 18:05:43 -0800

    Completed exercise 2.9 - added search functionality that filters the list of people by name

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,12 +1,16 @@
 import { useState } from 'react'
 import Person from './Components/Person'
+import Search from './Components/Search'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '734-343-2333' }
+    { name: 'Arto Hellas', number: '734-343-2333' },
+    { name: 'Peter Park', number: '432-234-6721'},
+    { name: 'Tony Stark', number: '123-531-4674'}
   ]) 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [newFilter, setNewFilter] = useState('')
 
   
   const addPerson = (event) => {
@@ -38,9 +42,20 @@ const App = () => {
     setNewNumber(event.target.value)
   }
 
+  const filteredPersons = persons.filter(person => person.name.toLowerCase().startsWith(newFilter.toLowerCase()))
+
+  let personDisplay= '';
+  if (newFilter === '') {
+    personDisplay = persons.map(person => <Person key={person.name} person={person} /> )
+  } else {
+    personDisplay = filteredPersons.map(person => <Person key={person.name} person={person} /> )
+  }
+  
   return (
     <div>
       <h2>Phonebook</h2>
+      <Search newFilter={newFilter} setNewFilter={setNewFilter}/> 
+      <h2>Add New</h2>
       <form onSubmit={addPerson}>
         <div>
           name: <input value={newName} onChange={handleNameChange} />
@@ -54,9 +69,7 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       <ul>
-        {persons.map(person =>  
-          <Person key={person.name} person={person} />
-        )}  
+        {personDisplay}
       </ul> 
     </div>
   )
---
+++ part2/phonebook/src/Components/Search.js
@@ -0,0 +1,14 @@
+const Search = ({newFilter, setNewFilter}) => {
+
+    const handleFilterChange = (event) => {
+        setNewFilter(event.target.value)
+      }
+    
+    return (
+        <div> 
+            filter shown with: <input value={newFilter} onChange={handleFilterChange}/> 
+        </div>
+    )
+}
+
+export default Search 
