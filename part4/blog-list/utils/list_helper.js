4d230d50bb4d5f6d4e946dc8ec693c1b842ddca8
Henry Winters <hlwinters17@gmail.com>
2022-04-23 20:31:01 -0700

    Completed exercise 4.5 - added unit test to determine the favorite blog based on number of likes

--- part4/blog-list/utils/list_helper.js
+++ part4/blog-list/utils/list_helper.js
@@ -10,7 +10,25 @@ const totalLikes = (blogs) => {
         }, 0)
 }
 
+const favoriteBlog = (blogs) => {
+    let mostLikes = 0;
+    let blogWithMostLikes = {}
+    for (let i = 0; i < blogs.length; i++) {
+        if (blogs[i].likes > mostLikes) {
+            mostLikes = blogs[i].likes 
+            blogWithMostLikes = {
+                title: blogs[i].title,
+                author: blogs[i].author,
+                likes: blogs[i].likes
+            }
+        }
+        console.log(blogWithMostLikes)
+    }
+    return blogWithMostLikes
+}
+
 module.exports = { 
     dummy, 
-    totalLikes
+    totalLikes, 
+    favoriteBlog
 }
