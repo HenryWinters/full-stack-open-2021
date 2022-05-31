82103479f71441f2d7e8212641b8e3056c175db9
Henry Winters <hlwinters17@gmail.com>
2022-05-30 23:28:21 -0700

    Completed exercise 5.7 - added button to each blog that toggles blog details visibility

--- part5/bloglist-frontend/src/components/Blog.js
+++ part5/bloglist-frontend/src/components/Blog.js
@@ -1,7 +1,35 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from 'react'
+import '../index.css'
+
+
+const Blog = ({ blog }) => {
+  const [visible, setVisible] = useState(false)
+
+  const hideWhenVisible = { display: visible ? 'none' : '' }
+  const showWhenVisible = { display: visible ? '' : 'none' }
+
+  const toggleVisibility = () => {
+    setVisible(!visible)
+  }
+
+  return (
+    <div className='blogs'>
+      <div>
+        <p> {blog.title} {blog.author} </p>
+        <div style={hideWhenVisible}>
+          <button onClick={toggleVisibility}>View</button>
+        </div>
+      </div>
+      <div style={showWhenVisible}> 
+        <p>URL: {blog.url}</p>
+        <div className='likeSection'>
+          <p>Likes: {blog.likes}</p>
+          <button className='likeButton'>Like</button>
+        </div>
+        <p>User: {blog.user[0].name}</p>
+        <button onClick={toggleVisibility}>Hide</button>
+      </div> 
+    </div>  
+)}
 
 export default Blog
