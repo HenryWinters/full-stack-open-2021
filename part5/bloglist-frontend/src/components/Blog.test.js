66a58de5c8616b895e5ae8ee616dfeb82ba7cf4b
Henry Winters <hlwinters17@gmail.com>
2022-07-13 21:30:56 -0700

    Completed exercise 5.14 - added test to check that blog's url and likes are shown when view button is clicked

--- part5/bloglist-frontend/src/components/Blog.test.js
+++ part5/bloglist-frontend/src/components/Blog.test.js
@@ -1,29 +1,45 @@
 import React from 'react'
 import '@testing-library/jest-dom/extend-expect'
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import Blog from './Blog'
 
-test('displays blog title and author only by default', () => {
+describe('blog displays', () => {
     let container 
 
     const blog = {
-      title: 'Component test title',
-      author: 'Component test author',
-      url: 'Component test url',
-      likes: 10,
-      user: 'Component test user'
-    }
+        title: 'Component test title',
+        author: 'Component test author',
+        url: 'Component test url',
+        likes: 10,
+        user: 'Component test user'
+      }
+  
+      const user = {
+          username: 'Component-test-user',
+          name: 'Component test user',
+          password: 'Test'
+      }
 
-    const user = {
-        username: 'Component-test-user',
-        name: 'Component test user',
-        password: 'Test'
-    }
+    beforeEach(() => {
+        container = render(<Blog blog={blog} user={user}/>).container
+    })
 
-    container = render(<Blog blog={blog} user={user}/>).container
+    test('title and author only by default', () => {
+        const defaultDiv = container.querySelector('.defaultBlogDisplay')
+        const fullDiv = container.querySelector('.fullBlogDisplay')
+        expect(defaultDiv).toHaveStyle('display: flex')
+        expect(fullDiv).toHaveStyle('display: none')
+    })
 
-    const defaultDiv = container.querySelector('.defaultBlogDisplay')
-    const fullDiv = container.querySelector('.fullBlogDisplay')
-    expect(defaultDiv).toHaveStyle('display: flex')
-    expect(fullDiv).toHaveStyle('display: none')
+    test('url and number of likes when button is clicked', async () => {
+        const user = userEvent.setup()
+        const button = screen.getByText('View')
+        await user.click(button)
+
+        const defaultDiv = container.querySelector('.defaultBlogDisplay')
+        const fullDiv = container.querySelector('.fullBlogDisplay')
+        expect(defaultDiv).toHaveStyle('display: none')
+        expect(fullDiv).toHaveStyle('display: block')
+    })
 })
