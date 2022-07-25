ccd0710a35de224b4a46f0f776619565cb521e6e
Henry Winters <hlwinters17@gmail.com>
2022-07-24 17:05:42 -0700

    Completed exercise 5.15 - added like button test that ensures the event handler is called the correct number of times

--- part5/bloglist-frontend/src/components/Blog.test.js
+++ part5/bloglist-frontend/src/components/Blog.test.js
@@ -3,6 +3,7 @@ import '@testing-library/jest-dom/extend-expect'
 import { render, screen } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import Blog from './Blog'
+import blogService from '../services/blogs'
 
 describe('blog displays', () => {
     let container 
@@ -43,3 +44,60 @@ describe('blog displays', () => {
         expect(fullDiv).toHaveStyle('display: block')
     })
 })
+
+/*test('clickling like button twice calls event handler twice', async () => {
+    const blog = {
+        title: 'Component test title',
+        author: 'Component test author',
+        url: 'Component test url',
+        likes: 10,
+        user: 'Component test user'
+      }
+  
+      const user = {
+          username: 'Component-test-user',
+          name: 'Component test user',
+          password: 'Test'
+      }
+
+    const mockHandler = jest.fn()
+
+    render(<Blog blog={blog} user={user} />)
+
+    const testUser = userEvent.setup()
+    const viewButton = screen.getByText('View')
+    await testUser.click(viewButton)
+    const likeButton = screen.getByText('Like')
+    await testUser.click(likeButton)
+    await testUser.click(likeButton)
+    expect(mockHandler.mock.calls).toHaveLength(2)
+})*/
+
+jest.mock('../services/blogs')
+
+test('clickling like button twice calls event handler twice', async () => {
+    const blog = {
+        title: 'Component test title',
+        author: 'Component test author',
+        url: 'Component test url',
+        likes: 10,
+        user: 'Component test user'
+      }
+  
+      const user = {
+          username: 'Component-test-user',
+          name: 'Component test user',
+          password: 'Test'
+      }
+
+   render(<Blog blog={blog} user={user} setBlogs={()=>console.log('')} />)
+
+   const testUser = userEvent.setup()
+   const viewButton = screen.getByText('View')
+   await testUser.click(viewButton)
+   const likeButton = screen.getByText('Like')
+   await testUser.click(likeButton)
+   await testUser.click(likeButton)
+   expect(blogService.addLikeToBlog).toBeCalledTimes(2)
+})
+
