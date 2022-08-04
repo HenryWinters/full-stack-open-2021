5a0a3285a7409007bfed77018a1930498ff93360
Henry Winters <hlwinters17@gmail.com>
2022-08-03 23:16:25 -0700

    Completed exercise 5.22 - made test that checks that blogs are ordered according to likes in descending order

--- part5/bloglist-frontend/cypress/e2e/blog_app.cy.js
+++ part5/bloglist-frontend/cypress/e2e/blog_app.cy.js
@@ -72,6 +72,36 @@ describe('Blog app', function() {
 
         cy.contains('Test Blog').should('not.exist')
       })
+
+      it.only('Blogs are ordered descending according to likes', function() {
+        cy.createBlog({
+          title: 'Test Blog 2',
+          author: 'Test Author 2',
+          url: 'Test Url',
+          likes: 300
+        })
+        cy.createBlog({
+          title: 'Test Blog 3',
+          author: 'Test Author 3',
+          url: 'Test Url 3',
+          likes: 200
+        })
+        cy.createBlog({
+          title: 'Test Blog 4',
+          author: 'Test Author 4',
+          url: 'Test Url 4',
+          likes: 400
+        })
+
+        cy.get('.blogs')
+          .then(($blogs) => Cypress._.map($blogs, (el) => el.innerHTML))
+          .then((list) => list.map((text) => text.match(/Likes:(.*?)<\/p>/)[1]))
+          .then((list) => list.map(parseFloat)) 
+          .then((list) => {
+            const sortedList = Cypress._.orderBy(list, 'desc')
+            expect(sortedList).to.deep.equal(list)
+          })
+      })
     })
   })
 })
